a20d4fb698b8d78b5e2b6a397c519fe37f9744fa
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-05-30 10:24:25 +0900

    refactor: 클래스 위치 변경

--- app/src/main/java/kr/cosine/groupfinder/presentation/view/search/SearchFragment.kt
+++ app/src/main/java/kr/cosine/groupfinder/presentation/view/search/SearchFragment.kt
@@ -1,10 +1,10 @@
-package kr.cosine.groupfinder.presentation
+package kr.cosine.groupfinder.presentation.view.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kr.cosine.groupfinder.R
 
 // TODO: Rename parameter arguments, choose names that match
