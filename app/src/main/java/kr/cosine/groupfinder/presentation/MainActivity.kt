dd634da8cbdfec8622b0aac529a6209bac697e3f
bora <bora44144@gmail.com>
2024-06-03 16:44:59 +0900

    feat: Navigation 기능 추가

--- app/src/main/java/kr/cosine/groupfinder/presentation/MainActivity.kt
+++ app/src/main/java/kr/cosine/groupfinder/presentation/MainActivity.kt
@@ -2,25 +2,37 @@ package kr.cosine.groupfinder.presentation
 
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+//import androidx.fragment.app.ListFragment
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
+import kr.cosine.groupfinder.R
 import kr.cosine.groupfinder.data.model.PostModel
 import kr.cosine.groupfinder.databinding.ActivityMainBinding
+import kr.cosine.groupfinder.presentation.view.list.ListFragment
+import kr.cosine.groupfinder.presentation.view.profile.ProfileFragment
 import kr.cosine.groupfinder.presentation.view.test.model.PostViewModel
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     private lateinit var firestore: FirebaseFirestore
 
     private val postViewModel by viewModels<PostViewModel>()
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +50,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
         registerView()
+        navigationSetting(savedInstanceState)
     }
 
     private fun registerView() {
@@ -49,6 +62,60 @@ class MainActivity : AppCompatActivity() {
                     Log.d("GroupFinderTest2", it.toString())
                 }
             }
+        }
+    }
+
+    private fun navigationSetting(savedInstanceState: Bundle?) {
+        drawerLayout = binding.drawerLayout
+        val toolbar = binding.toolbar
+        val navigationView = binding.navigationView
+
+        setSupportActionBar(toolbar)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_navigation,
+            R.string.close_navigation
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ListFragment()).commit()
+            navigationView.setCheckedItem(R.id.navigation_All)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.navigation_All -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ListFragment()).commit()
+            }
+            R.id.navigation_Normal -> Toast.makeText(this, "일반", Toast.LENGTH_SHORT).show()
+            R.id.navigation_Wind -> Toast.makeText(this, "칼바람 나락", Toast.LENGTH_SHORT).show()
+            R.id.navigation_Solo -> Toast.makeText(this, "솔로/듀오 랭크", Toast.LENGTH_SHORT).show()
+            R.id.navigation_Free -> Toast.makeText(this, "자유랭크", Toast.LENGTH_SHORT).show()
+            R.id.navigation_myProfile -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, ProfileFragment()).commit()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
