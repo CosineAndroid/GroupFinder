41e8ade79f1b20b9afce9a8495eb3c4d43662373
HOCRUX\penci <pencil2486@gmail.com>
2024-06-10 19:18:51 +0900

    [#7] fix: 미사용 파일 삭제

--- app/src/main/java/kr/cosine/groupfinder/presentation/view/detail/item/LaneItem.kt
+++
@@ -1,7 +0,0 @@
-package kr.cosine.groupfinder.presentation.view.detail.item
-
-data class LaneItem(
-    val uniqueId: String,
-    val nickname: String,
-    val tag: String
-)
--- app/src/main/java/kr/cosine/groupfinder/presentation/view/detail/item/OwnerItem.kt
+++
@@ -1,9 +0,0 @@
-package kr.cosine.groupfinder.presentation.view.detail.item
-
-import java.util.UUID
-
-data class OwnerItem(
-    val uniqueId: UUID,
-    val nickname: String,
-    val tag: String
-)
