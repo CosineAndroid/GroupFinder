3cae902e61caa8c12fc6838dcc6c699e31d6e4f9
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-06-10 11:05:50 +0900

    [#4] feat: 방 참여 중일 때 탈퇴 안되게 변경

--- app/src/main/java/kr/cosine/groupfinder/domain/usecase/WithdrawAccountUseCase.kt
+++ app/src/main/java/kr/cosine/groupfinder/domain/usecase/WithdrawAccountUseCase.kt
@@ -1,17 +1,23 @@
 package kr.cosine.groupfinder.domain.usecase
 
 import dagger.hilt.android.scopes.ViewModelScoped
+import kr.cosine.groupfinder.domain.exception.WithdrawWithJoinException
 import kr.cosine.groupfinder.domain.repository.AccountRepository
+import kr.cosine.groupfinder.domain.repository.PostRepository
 import java.util.UUID
 import javax.inject.Inject
 
 @ViewModelScoped
 class WithdrawAccountUseCase @Inject constructor(
-    private val accountRepository: AccountRepository
+    private val accountRepository: AccountRepository,
+    private val postRepository: PostRepository
 ) {
 
     suspend operator fun invoke(uniqueId: UUID): Result<Unit> {
         return runCatching {
+            if (postRepository.isJoined(uniqueId)) {
+                throw WithdrawWithJoinException()
+            }
             accountRepository.deleteAccountByUniqueId(uniqueId)
         }
     }
--- app/src/main/java/kr/cosine/groupfinder/presentation/view/profile/event/ProfileEvent.kt
+++ app/src/main/java/kr/cosine/groupfinder/presentation/view/profile/event/ProfileEvent.kt
@@ -6,6 +6,8 @@ sealed interface ProfileEvent {
         val message: String
     ) : ProfileEvent
 
+    data object JoinFail : Notice("방을 참가한 상태에서 탈퇴할 수 없습니다.")
+
     data object UnknownFail : Notice("알 수 없는 오류로 실패하였습니다.")
 
     data object Success : ProfileEvent
