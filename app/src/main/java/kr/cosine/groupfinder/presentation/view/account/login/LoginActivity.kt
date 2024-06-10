236d511f78bcb366f3012720d768366aa744d0e7
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-06-10 19:58:43 +0900

    [#21] feat: 테스트 코드 제거

--- app/src/main/java/kr/cosine/groupfinder/presentation/view/account/login/LoginActivity.kt
+++ app/src/main/java/kr/cosine/groupfinder/presentation/view/account/login/LoginActivity.kt
@@ -1,28 +1,18 @@
 package kr.cosine.groupfinder.presentation.view.account.login
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
 import kr.cosine.groupfinder.presentation.view.account.login.screen.LoginScreen
-import kr.cosine.groupfinder.presentation.view.common.data.IntentKey
-import kr.cosine.groupfinder.presentation.view.record.RecordActivity
 
 @AndroidEntryPoint
 class LoginActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val intent = Intent(this, RecordActivity::class.java).apply {
-            putExtra(IntentKey.NICKNAME, "hide on bush")
-            putExtra(IntentKey.TAG, "kr1")
-        }
-        startActivity(intent)
-
         setContent {
-            // LoginScreen()
+            LoginScreen()
         }
     }
 }
