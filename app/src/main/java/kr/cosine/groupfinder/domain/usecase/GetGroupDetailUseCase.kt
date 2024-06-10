ed92a450ba60097bd3e1a11fba38a95a152ab525
HOCRUX\penci <pencil2486@gmail.com>
2024-06-10 19:18:29 +0900

    [#7] fix: 미사용 import 삭제

--- app/src/main/java/kr/cosine/groupfinder/domain/usecase/GetGroupDetailUseCase.kt
+++ app/src/main/java/kr/cosine/groupfinder/domain/usecase/GetGroupDetailUseCase.kt
@@ -1,12 +1,9 @@
 package kr.cosine.groupfinder.domain.usecase
 
 import dagger.hilt.android.scopes.ViewModelScoped
-import kr.cosine.groupfinder.data.model.GroupDetailResponse
 import kr.cosine.groupfinder.domain.mapper.toEntity
 import kr.cosine.groupfinder.domain.model.GroupDetailEntity
 import kr.cosine.groupfinder.domain.repository.GroupRepository
-import kr.cosine.groupfinder.enums.Mode
-import kr.cosine.groupfinder.presentation.view.detail.item.GroupDetailItem
 import java.util.UUID
 import javax.inject.Inject
 
