16bbbb8ca8a615a86b48df8463b3f1e3350705ea
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-06-20 10:29:20 +0900

    feat: 공지 액티비티 기본 틀 추가

---
+++ app/src/main/java/kr/cosine/groupfinder/presentation/view/broadcast/BroadcastActivity.kt
@@ -0,0 +1,14 @@
+package kr.cosine.groupfinder.presentation.view.broadcast
+
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.cosine.groupfinder.presentation.view.common.GroupFinderActivity
+
+@AndroidEntryPoint
+class BroadcastActivity : GroupFinderActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
